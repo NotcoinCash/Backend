a2e6efc3ea3a120a210bde0ff9cef382ca9bf424
Mikhaylo Kisil <mihajlokisil20@gmail.com>
2024-11-01 18:22:09 +0200

    Set up CORS for local development

--- src/main.py
+++ src/main.py
@@ -12,14 +12,14 @@ init_admin(app)
 #     "http://localhost",
 #     "http://localhost:8000",
 # ]
-#
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
